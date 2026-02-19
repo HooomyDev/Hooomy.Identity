606f300e701d28b54fff62de2d1113f811fb94cc
artxmix <artem.artusevskij01@gmail.com>
2026-02-19 22:37:38 +0300

    feat: Add AdminController

---
+++ Hooome.Identity/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Hooome.Identity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hooome.Identity.Controllers;
+
+[ApiController]
+[Route("admin")]
+public class AdminController(
+    UserManager<AppUser> userManager) : ControllerBase
+
+{
+    [HttpGet("users/count")]
+    public async Task<ActionResult<int>> GetUsersCount()
+    {
+        var count = await userManager.Users.CountAsync();
+        
+        return Ok(count);
+    }
+}
